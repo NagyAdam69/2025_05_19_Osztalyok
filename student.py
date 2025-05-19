c8d314b92fd6fd6bddbdac552585a2f617c5c175
Nagy Ádám <nagy.adam@diak.szbi-pg.hu>
2025-05-19 10:27:03 +0200

    6. Leila default age értékkel
    age paramétert az utolsó értéknek kellett megadnunk

--- student.py
+++ student.py
@@ -1,19 +1,23 @@
 class Student: 
     # konstruktor az osztály példányosításához
-    def __init__(self, name, age, sex):
+    def __init__(self, name, sex, mood, age = "Nincs megadva"):
         self.name = name
         self.age = age
         self.sex = sex
         self.score = 10
+        self.mood = mood
 
     def introduce(self):
-        print(f"\nNév: {self.name} \nKor: {self.age} \nPontszám: {self.score} \n")
+        print(f"\nNév: {self.name} \nKor: {self.age} \nPontszám: {self.score} \nHangulat: {self.mood}\n")
         
     def studied(self, points):
         self.score += points
 
-tivadar = Student("El Tivadar", 16, "male")
+tivadar = Student("El Tivadar", 16, "male", "Stresszelt")
+leila = Student("Leila hercegnő", "female", "Tired")
 
 tivadar.introduce()
 tivadar.studied(12)
 tivadar.introduce()
+
+leila.introduce()
