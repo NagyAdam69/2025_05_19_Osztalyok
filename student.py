757d577bce50cb32a072bb297e6e8e19fed7dcb9
Nagy Ádám <nagy.adam@diak.szbi-pg.hu>
2025-05-19 09:32:39 +0200

    2. Adatok felvétele

--- student.py
+++ student.py
@@ -6,3 +6,10 @@ class Student:
 
 tivadar = Student()
 print(tivadar)
+
+tivadar.name = "El Tivadar"
+tivadar.age = 16
+tivadar.sex = "male"
+tivadar.score = 20
+
+print(f"Név: {tivadar.name} \nKor: {tivadar.age} \nPontszám: {tivadar.score}")
